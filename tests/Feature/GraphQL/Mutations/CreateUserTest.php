aa16cd61328dfd1049a2681f57a30642dc3a92c6
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-07-30 21:31:50 +0900

    同じemailで登録しようとすると弾かれるようにした

--- tests/Feature/GraphQL/Mutations/CreateUserTest.php
+++ tests/Feature/GraphQL/Mutations/CreateUserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 test('ユーザー作成ができる', function ()
 {
     $input = [
@@ -34,5 +36,40 @@ test('ユーザー作成ができる', function ()
         'id' => $response['data']['createUser']['record']['id'],
         'name' => $input['input']['name'],
         'email' => $input['input']['email'],
+    ]);
+});
+
+test('同じemailで登録しようとすると弾かれる', function () {
+    $user = User::factory()->create();
+
+    $input = [
+        'input' => [
+            'name' => 'sampleUser',
+            'email' => $user->email,
+            'password' => 'password',
+        ]
+    ];
+    $response = $this->graphQL(/** @lang GraphQL */'
+        mutation CreateUser($input: CreateUserInput!) {
+            createUser(
+                input: $input
+            ) {
+                record {
+                    id
+                }
+            }
+        }',$input);
+
+    $response->assertJsonStructure([
+        'errors' => [
+            [
+                'message',
+                'extensions' => [
+                    'validation' => [
+                        'input.email',
+                    ],
+                ],
+            ],
+        ],
     ]);
 });
