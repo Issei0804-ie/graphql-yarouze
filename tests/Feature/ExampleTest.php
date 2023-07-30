41e92190466ceaf954c3da6df92f519929ffae3c
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-07-30 21:00:42 +0900

    pestに変えた

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,31 +1,15 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
-use PHPUnit\Framework\Attributes\Test;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
+test('ユーザー作成ができる', function ()
 {
-    use RefreshDatabase;
-    use MakesGraphQLRequests;
-    /**
-     * A basic test example.
-     */
-    #[test]
-    public function ユーザー作成ができる()
-    {
-        $input = [
-            'input' => [
-                'name' => 'sampleUser',
-                'email' => 'example@example.com',
-                'password' => 'password',
-            ]
-        ];
-        $response = $this->graphQL(/** @lang GraphQL */'
+    $input = [
+        'input' => [
+            'name' => 'sampleUser',
+            'email' => 'example@example.com',
+            'password' => 'password',
+        ]
+    ];
+    $response = $this->graphQL(/** @lang GraphQL */'
         mutation CreateUser($input: CreateUserInput!) {
             createUser(
                 input: $input
@@ -36,20 +20,19 @@ class ExampleTest extends TestCase
             }
         }',$input);
 
-        $response->assertJsonStructure([
-            'data' => [
-                'createUser' => [
-                    'record' => [
-                        'id',
-                    ],
+    $response->assertJsonStructure([
+        'data' => [
+            'createUser' => [
+                'record' => [
+                    'id',
                 ],
             ],
-        ]);
+        ],
+    ]);
 
-        $this->assertDatabaseHas('users', [
-            'id' => $response['data']['createUser']['record']['id'],
-            'name' => $input['input']['name'],
-            'email' => $input['input']['email'],
-        ]);
-    }
-}
+    $this->assertDatabaseHas('users', [
+        'id' => $response['data']['createUser']['record']['id'],
+        'name' => $input['input']['name'],
+        'email' => $input['input']['email'],
+    ]);
+});
