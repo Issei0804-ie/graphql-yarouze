f8d81fe56e64d04b3deebc1903116246dbd14011
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-07-30 19:47:44 +0900

    waiwai

--- app/GraphQL/Mutations/CreateUser.php
+++ app/GraphQL/Mutations/CreateUser.php
@@ -7,10 +7,12 @@ use App\Models\User;
 class CreateUser {
     public function __invoke($_, array $args)
     {
-        return User::create([
+        $user = User::create([
             'name' => $args['name'],
             'email' => $args['email'],
             'password' => \Hash::make($args['password']),
         ]);
+        logger()->info('user ', ['user' => $user]);
+        return ['record' => $user];
     }
 }
