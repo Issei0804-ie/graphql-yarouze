566daeed099b3de34b9925622ceebe5b5e4eb8b6
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-07-30 20:57:45 +0900

    test書いた

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,10 +1,55 @@
 <?php
 
-it(
-    'returns a successful response',
-    function () {
-        $response = $this->get('/');
+namespace Tests\Feature;
 
-        $response->assertStatus(200);
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+    use MakesGraphQLRequests;
+    /**
+     * A basic test example.
+     */
+    #[test]
+    public function ユーザー作成ができる()
+    {
+        $input = [
+            'input' => [
+                'name' => 'sampleUser',
+                'email' => 'example@example.com',
+                'password' => 'password',
+            ]
+        ];
+        $response = $this->graphQL(/** @lang GraphQL */'
+        mutation CreateUser($input: CreateUserInput!) {
+            createUser(
+                input: $input
+            ) {
+                record {
+                    id
+                }
+            }
+        }',$input);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'createUser' => [
+                    'record' => [
+                        'id',
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $response['data']['createUser']['record']['id'],
+            'name' => $input['input']['name'],
+            'email' => $input['input']['email'],
+        ]);
     }
-);
+}
